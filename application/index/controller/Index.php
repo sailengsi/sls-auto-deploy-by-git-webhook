2a1ed15feb2e27168e7707995f6f86029295e81d
赛冷思 <silence@sailengsi.com>
2017-12-01 20:45:28 +0800

    测试hook

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -6,7 +6,16 @@ use app\index\model\Log;
 
 class Index {
     public function index() {
+        $requestBody = file_get_contents("php://input");
+        $data        = json_decode($requestBody, true);
         $LogModel = new Log();
-        return $LogModel->select();
+        if (empty($data)) {
+            return $LogModel->select();
+        }
+        $data=[
+            'content'=>$requestBody
+        ];
+        $LogModel->save($data);
+        return $LogModel->id;
     }
 }
